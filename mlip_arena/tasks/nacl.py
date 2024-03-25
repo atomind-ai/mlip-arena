ab8fd251743f892d66faf324bcb67eeabdebcbb0
Yuan Chiang <qaqowotut@gmail.com>
2024-03-25 16:04:22 -0700

    test streamlit

--- mlip_arena/tasks/nacl.py
+++ mlip_arena/tasks/nacl.py
@@ -1,5 +1,7 @@
 
+import yaml
 from torch_dftd.torch_dftd3_calculator import TorchDFTD3Calculator
 
 from mlip_arena.models import REGISTRY_FILE
 
+yaml.load(open(REGISTRY_FILE), Loader=yaml.FullLoader)
