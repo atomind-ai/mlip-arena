d98d70110886a32787a2d37bf0d2cc0373d20b44
Yuan Chiang <cyrusyc@berkeley.edu>
2024-03-29 21:45:25 -0700

    improve class defintion and new task
    - Improve MLIP, MLIPCalculator class definitions
    - Add diatomics.py task
    - Introduce covalent, improve pyproject.toml

--- mlip_arena/models/__init__.py
+++ mlip_arena/models/__init__.py
@@ -1,27 +1,49 @@
 import os
+from pathlib import Path
 
 import torch
 import yaml
 from ase import Atoms
 from ase.calculators.calculator import Calculator, all_changes
+from huggingface_hub import PyTorchModelHubMixin
 from torch import nn
 from torch_geometric.data import Data
 
 with open(os.path.join(os.path.dirname(__file__), "registry.yaml")) as f:
     REGISTRY = yaml.load(f, Loader=yaml.FullLoader)
 
-class MLIP(Calculator):
-    def __init__(self, 
-                 model_path: str = None,
-                 device: torch.device = None):
+# class MLIPEnum(enum.Enum):
+#     for model, metadata in REGISTRY.items():
+#         model_class = getattr(importlib.import_module(model["module"]), model)
+#         self.setattr(model, model_class)
+
+
+class MLIP(
+    nn.Module,
+    PyTorchModelHubMixin,
+    tags=["atomistic-simulation", "MLIP"],
+):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+
+class MLIPCalculator(Calculator):
+    def __init__(
+        self,
+        model_path: str | Path,
+        device: torch.device | None = None,
+    ):
         super().__init__()
         self.name: str = self.__class__.__name__
-        self.version: str = None
-        self.device = device or torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model: nn.Module = torch.load(model_path, map_location=self.device)
-        self.implemented_properties = ["energy", "forces"]
+        self.device = device or torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )
+        self.model: MLIP = MLIP.from_pretrained(model_path, map_location=self.device)
+        self.implemented_properties = ["energy", "forces", "stress"]
 
-    def calculate(self, atoms: Atoms, properties: list[str], system_changes: dict = all_changes):
+    def calculate(
+        self, atoms: Atoms, properties: list[str], system_changes: list = all_changes
+    ):
         """Calculate energies and forces for the given Atoms object"""
         super().calculate(atoms, properties, system_changes)
 
@@ -29,13 +51,15 @@ class MLIP(Calculator):
 
         self.results = {}
         if "energy" in properties:
-            self.results["energy"] = output["energy"].item()
+            self.results["energy"] = output["energy"].squeeze().item()
         if "forces" in properties:
-            self.results["forces"] = output["forces"].cpu().detach().numpy()
+            self.results["forces"] = output["forces"].squeeze().cpu().detach().numpy()
+        if "stress" in properties:
+            self.results["stress"] = output["stress"].squeeze().cpu().detach().numpy()
 
-    def forward(self, x: Data | Atoms) -> dict[str, torch.Tensor]:
+    def forward(self, x: Atoms) -> dict[str, torch.Tensor]:
         """Implement data conversion, graph creation, and model forward pass
-        
+
         Example implementation:
         1. Use `ase.neighborlist.NeighborList` to get neighbor list
         2. Create `torch_geometric.data.Data` object and copy the data
--- mlip_arena/tasks/__init__.py
+++ mlip_arena/tasks/__init__.py
@@ -12,33 +12,42 @@ with open(os.path.join(os.path.dirname(__file__), "registry.yaml")) as f:
 
 class Task:
     def __init__(self):
-        self.name: str = self.__class__.__name__ # display name on the leaderboard
+        self.name: str = self.__class__.__name__  # display name on the leaderboard
 
-    def run(self, model: MLIP):
+    def run_local(self, model: MLIP):
         """Run the task using the given model and return the results"""
         raise NotImplementedError
-    
+
+    def run_hf(self, model: MLIP):
+        """Run the task using the given model and return the results"""
+        raise NotImplementedError
+
         # Calcualte evaluation metrics and postprocessed data
         api = HfApi()
         api.upload_file(
             path_or_fileobj="results.json",
-            path_in_repo=f"{self.__class__.__name__}/{model.__class__.__name__}/results.json", # Upload to a specific folder
+            path_in_repo=f"{self.__class__.__name__}/{model.__class__.__name__}/results.json",  # Upload to a specific folder
             repo_id="atomind/mlip-arena",
-            repo_type="dataset"
+            repo_type="dataset",
         )
-    
+
+    def run_nersc(self, model: MLIP):
+        """Run the task using the given model and return the results"""
+        raise NotImplementedError
+
     def get_results(self):
         """Get the results from the task"""
 
         # fs = HfFileSystem()
         # files = fs.glob(f"datasets/atomind/mlip-arena/{self.__class__.__name__}/*/*.json")
-        
+
         for model, metadata in MODEL_REGISTRY.items():
             results = hf_hub_download(
-                repo_id="atomind/mlip-arena", filename="results.json", 
-                subfolder=f"{self.__class__.__name__}/{model}", 
-                repo_type="dataset", 
-                revision=None
-                )
-        
+                repo_id="atomind/mlip-arena",
+                filename="results.json",
+                subfolder=f"{self.__class__.__name__}/{model}",
+                repo_type="dataset",
+                revision=None,
+            )
+
         return results
