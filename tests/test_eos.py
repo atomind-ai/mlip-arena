c3e45aebeece8b1076ecc930b4af336134efd845
Yuan Chiang <cyrusyc@berkeley.edu>
2024-10-07 22:33:13 -0700

    refactor test eos

--- tests/test_eos.py
+++ tests/test_eos.py
@@ -1,18 +1,20 @@
-import numpy as np
+import pytest
 from ase.build import bulk
 
+from mlip_arena.models.utils import MLIPEnum
 from mlip_arena.tasks.eos.run import fit as EOS
 
 atoms = bulk("Cu", "fcc", a=3.6)
 
-def test_eos():
+@pytest.mark.parametrize("model", [MLIPEnum["MACE-MP(M)"]])
+def test_eos(model: MLIPEnum):
     """
     Test EOS prefect workflow with a simple cubic lattice.
     """
 
     result = EOS(
         atoms=atoms,
-        calculator_name="MACE-MP(M)",
+        calculator_name=model.name,
         calculator_kwargs={},
         device=None,
         optimizer="BFGSLineSearch",
@@ -20,10 +22,10 @@ def test_eos():
         filter="FrechetCell",
         filter_kwargs=None,
         criterion=dict(
-            fmax=0.01,
+            fmax=0.1,
         ),
         max_abs_strain=0.1,
-        npoints=11,
+        npoints=6,
     )
 
     print(result)
