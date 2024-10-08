e62a800e546d871814009d436e4a841ea9fac942
Yuan Chiang <cyrusyc@berkeley.edu>
2024-10-07 22:53:04 -0700

    add pytest skip reason

--- tests/test_eos.py
+++ tests/test_eos.py
@@ -8,7 +8,7 @@ from mlip_arena.tasks.eos.run import fit as EOS
 atoms = bulk("Cu", "fcc", a=3.6)
 
 @pytest.mark.parametrize("model", [MLIPEnum["MACE-MP(M)"]])
-@pytest.mark.skipif(sys.version_info != (3, 11))
+@pytest.mark.skipif(sys.version_info != (3, 11), reason="requires Python 3.11 to use prefect")
 def test_eos(model: MLIPEnum):
     """
     Test EOS prefect workflow with a simple cubic lattice.
