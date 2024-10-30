f8901df4e72f38ea9405ae16c4e1790c0aa62cd2
Yuan Chiang <cyrusyc@berkeley.edu>
2024-10-29 18:26:52 -0700

    add thermal conductivity rank

--- serve/ranks/thermal-conductivity.py
+++ serve/ranks/thermal-conductivity.py
@@ -1,7 +1,32 @@
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 import streamlit as st
 
-from mlip_arena.models import REGISTRY as MODELS
+DATA_DIR = Path("mlip_arena/tasks/thermal-conductivity")
+
+table = pd.read_csv(DATA_DIR / "wte.csv")
+
+table.rename(
+    columns={
+        "method": "Model",
+        "srme": "SRME [W/mK]",
+    },
+    inplace=True,
+)
+
+table.set_index("Model", inplace=True)
+
+table.sort_values(["SRME [W/mK]"], ascending=True, inplace=True)
+
+s = table.style.background_gradient(
+    cmap="viridis_r", subset=["SRME [W/mK]"]
+)
+
+
+def render():
+
+    st.dataframe(
+        s,
+        use_container_width=True
+    )
