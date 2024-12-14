aeaedb42e2b66af56f1eeb8e6835f7570e099825
Yuan Chiang <cyrusyc@lbl.gov>
2024-12-13 18:10:55 -0800

    avoid update torch during dgl installation

--- scripts/install-dgl.sh
+++ scripts/install-dgl.sh
@@ -3,4 +3,4 @@
 TORCH=2.2
 CUDA=cu121
 
-pip install dgl -U -f https://data.dgl.ai/wheels/torch-${TORCH}/${CUDA}/repo.html
+pip install dgl -f https://data.dgl.ai/wheels/torch-${TORCH}/${CUDA}/repo.html
