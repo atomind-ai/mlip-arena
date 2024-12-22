3fc9a24d3589b1222ef7860617e7445be779afb7
Yuan Chiang <cyrusyc@berkeley.edu>
2024-12-22 10:45:00 -0800

    fix generator

--- tests/test_mof.py
+++ tests/test_mof.py
@@ -9,17 +9,18 @@ from ase.build import molecule
 
 @pytest.mark.parametrize("model", [MLIPEnum["MACE-MP(M)"]])
 def test_widom_insertion(model: MLIPEnum):
-    atoms = get_atoms_from_db("mof.db")
     with prefect_test_harness():
-        result = widom_insertion.with_options(
-            refresh_cache=True,
-        )(
-            structure=atoms,
-            gas=molecule("CO2"),
-            calculator_name=model.name,
-            num_insertions=10,
-        )
-        assert isinstance(result, dict)
-        assert isinstance(result["henry_coefficient"][0], float)
-        assert isinstance(result["averaged_interaction_energy"][0], float)
-        assert isinstance(result["heat_of_adsorption"][0], float)
+        for atoms in get_atoms_from_db("mofs.db"):
+            result = widom_insertion.with_options(
+                refresh_cache=True,
+            )(
+                structure=atoms,
+                gas=molecule("CO2"),
+                calculator_name=model.name,
+                num_insertions=10,
+            )
+            assert isinstance(result, dict)
+            assert isinstance(result["henry_coefficient"][0], float)
+            assert isinstance(result["averaged_interaction_energy"][0], float)
+            assert isinstance(result["heat_of_adsorption"][0], float)
+            break # only test one MOF
