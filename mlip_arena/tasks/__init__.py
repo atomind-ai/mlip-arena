54047c6749c3bfd92582818634b547ff80e6d73c
Yuan (Cyrus) Chiang <41962462+chiang-yuan@users.noreply.github.com>
2025-01-17 09:18:59 -0800

    hotfix mattersim pickling issue; add phonon task (#42)

--- mlip_arena/tasks/__init__.py
+++ mlip_arena/tasks/__init__.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import yaml
 from huggingface_hub import HfApi, HfFileSystem, hf_hub_download
 
-from mlip_arena.models import MLIP
-from mlip_arena.models import REGISTRY as MODEL_REGISTRY
+# from mlip_arena.models import MLIP
+# from mlip_arena.models import REGISTRY as MODEL_REGISTRY
 
 try:
     from .elasticity import run as ELASTICITY
@@ -13,8 +13,9 @@ try:
     from .neb import run as NEB
     from .neb import run_from_endpoints as NEB_FROM_ENDPOINTS
     from .optimize import run as OPT
+    from .phonon import run as PHONON
 
-    __all__ = ["OPT", "EOS", "MD", "NEB", "NEB_FROM_ENDPOINTS", "ELASTICITY"]
+    __all__ = ["OPT", "EOS", "MD", "NEB", "NEB_FROM_ENDPOINTS", "ELASTICITY", "PHONON"]
 except ImportError:
     pass
 
@@ -22,43 +23,43 @@ with open(Path(__file__).parent / "registry.yaml", encoding="utf-8") as f:
     REGISTRY = yaml.safe_load(f)
 
 
-class Task:
-    def __init__(self):
-        self.name: str = self.__class__.__name__  # display name on the leaderboard
+# class Task:
+#     def __init__(self):
+#         self.name: str = self.__class__.__name__  # display name on the leaderboard
 
-    def run_local(self, model: MLIP):
-        """Run the task using the given model and return the results."""
-        raise NotImplementedError
+#     def run_local(self, model: MLIP):
+#         """Run the task using the given model and return the results."""
+#         raise NotImplementedError
 
-    def run_hf(self, model: MLIP):
-        """Run the task using the given model and return the results."""
-        raise NotImplementedError
+#     def run_hf(self, model: MLIP):
+#         """Run the task using the given model and return the results."""
+#         raise NotImplementedError
 
-        # Calcualte evaluation metrics and postprocessed data
-        api = HfApi()
-        api.upload_file(
-            path_or_fileobj="results.json",
-            path_in_repo=f"{self.__class__.__name__}/{model.__class__.__name__}/results.json",  # Upload to a specific folder
-            repo_id="atomind/mlip-arena",
-            repo_type="dataset",
-        )
+#         # Calcualte evaluation metrics and postprocessed data
+#         api = HfApi()
+#         api.upload_file(
+#             path_or_fileobj="results.json",
+#             path_in_repo=f"{self.__class__.__name__}/{model.__class__.__name__}/results.json",  # Upload to a specific folder
+#             repo_id="atomind/mlip-arena",
+#             repo_type="dataset",
+#         )
 
-    def run_nersc(self, model: MLIP):
-        """Run the task using the given model and return the results."""
-        raise NotImplementedError
+#     def run_nersc(self, model: MLIP):
+#         """Run the task using the given model and return the results."""
+#         raise NotImplementedError
 
-    def get_results(self):
-        """Get the results from the task."""
-        # fs = HfFileSystem()
-        # files = fs.glob(f"datasets/atomind/mlip-arena/{self.__class__.__name__}/*/*.json")
+#     def get_results(self):
+#         """Get the results from the task."""
+#         # fs = HfFileSystem()
+#         # files = fs.glob(f"datasets/atomind/mlip-arena/{self.__class__.__name__}/*/*.json")
 
-        for model, metadata in MODEL_REGISTRY.items():
-            results = hf_hub_download(
-                repo_id="atomind/mlip-arena",
-                filename="results.json",
-                subfolder=f"{self.__class__.__name__}/{model}",
-                repo_type="dataset",
-                revision=None,
-            )
+#         for model, metadata in MODEL_REGISTRY.items():
+#             results = hf_hub_download(
+#                 repo_id="atomind/mlip-arena",
+#                 filename="results.json",
+#                 subfolder=f"{self.__class__.__name__}/{model}",
+#                 repo_type="dataset",
+#                 revision=None,
+#             )
 
-        return results
+#         return results
