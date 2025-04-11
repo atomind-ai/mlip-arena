228438d03b462c58127776b6a55fc95b0ea2e958
Yuan Chiang <cyrusyc@berkeley.edu>
2025-04-10 22:24:55 -0700

    add hydrogen combution rank

--- serve/app.py
+++ serve/app.py
@@ -4,21 +4,6 @@ import streamlit as st
 
 from mlip_arena.tasks import REGISTRY as TASKS
 
-# if "logged_in" not in st.session_state:
-#     st.session_state.logged_in = False
-
-# def login():
-#     if st.button("Log in"):
-#         st.session_state.logged_in = True
-#         st.rerun()
-
-# def logout():
-#     if st.button("Log out"):
-#         st.session_state.logged_in = False
-#         st.rerun()
-
-# login_page = st.Page(login, title="Log in", icon=":material/login:")
-# logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 
 leaderboard = st.Page(
     "leaderboard.py", title="Leaderboard", icon=":material/trophy:", default=True
