c23def4c34e88b4141a6c9f98b31d8d573a29b8c
Yuan Chiang <cyrusyc@berkeley.edu>
2025-04-14 09:21:21 -0700

    add bulk eos parquets

--- examples/eos_bulk/run.py
+++ examples/eos_bulk/run.py
@@ -12,6 +12,7 @@ from prefect_dask import DaskTaskRunner
 
 from mlip_arena.models import REGISTRY, MLIPEnum
 from mlip_arena.tasks.eos import run as EOS
+from mlip_arena.tasks.optimize import run as OPT
 from mlip_arena.tasks.utils import get_calculator
 
 
@@ -38,36 +39,59 @@ def save_result(
 
     result["method"] = model_name
     result["id"] = id
+    result.pop("atoms", None)
 
-    fpath = Path(f"{model_name}.pkl")
+    # fpath = Path(f"{model_name}.parquet")
+    fpath = Path(f"{model_name}")
+    fpath.mkdir(exist_ok=True)
 
-    if fpath.exists():
-        df = pd.read_parquet(fpath)
-        df = pd.concat([df, pd.DataFrame([result])], ignore_index=True)
-    else:
-        df = pd.DataFrame([result])
+    fpath = fpath / f"{result['id']}.pkl"
 
-    df.drop_duplicates(subset=["id", "model"], keep="last", inplace=True)
-    df.to_parquet(fpath)
+    df = pd.DataFrame([result])
+    df.to_pickle(fpath)
+
+    # if fpath.exists():
+    #     df = pd.read_parquet(fpath)
+    #     df = pd.concat([df, pd.DataFrame([result])], ignore_index=True)
+    # else:
+    #     df = pd.DataFrame([result])
+
+    # df.drop_duplicates(subset=["id", "method"], keep="last", inplace=True)
+    # df.to_parquet(fpath)
 
 
 @task
 def eos_bulk(atoms, model):
+
+    calculator = get_calculator(
+        model
+    )  # avoid sending entire model over prefect and select freer GPU
+
+    result = OPT.with_options(
+        refresh_cache=True,
+    )(
+        atoms,
+        calculator,
+        optimizer="FIRE",
+        criterion=dict(
+            fmax=0.1,
+        ),
+    )
+
     return EOS.with_options(
-        refresh_cache=False,
+        refresh_cache=True,
         on_completion=[functools.partial(
             save_result,
             model_name=model.name,
             id=atoms.info["key_value_pairs"]["wbm_id"],
         )],
     )(
-        atoms,
-        calculator=get_calculator(
-            model
-        ),  # avoid sending entire model over prefect and select freer GPU
+        atoms=result["atoms"],
+        calculator=calculator,
         optimizer="FIRE",
         npoints=21,
         max_abs_strain=0.2,
+        concurrent=False
     )
 
 
@@ -92,7 +116,7 @@ cluster_kwargs = dict(
     memory="64 GB",
     shebang="#!/bin/bash",
     account="m3828",
-    walltime="00:30:00",
+    walltime="00:50:00",
     job_mem="0",
     job_script_prologue=[
         "source ~/.bashrc",
@@ -102,7 +126,7 @@ cluster_kwargs = dict(
     job_directives_skip=["-n", "--cpus-per-task", "-J"],
     job_extra_directives=[
         "-J eos-bulk",
-        "-q debug",
+        "-q regular",
         f"-N {nodes_per_alloc}",
         "-C gpu",
         f"-G {gpus_per_alloc}",
@@ -112,10 +136,10 @@ cluster_kwargs = dict(
 
 cluster = SLURMCluster(**cluster_kwargs)
 print(cluster.job_script())
-cluster.adapt(minimum_jobs=2, maximum_jobs=2)
+cluster.adapt(minimum_jobs=20, maximum_jobs=40)
 client = Client(cluster)
 
 run_all.with_options(
     task_runner=DaskTaskRunner(address=client.scheduler.address),
-    log_prints=False,
+    log_prints=True,
 )()
