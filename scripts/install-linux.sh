d960d04ca439008ae286e201d95744e92f1523ef
Yuan Chiang <cyrusyc@berkeley.edu>
2025-04-28 19:04:24 -0700

    update installation script

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -1,5 +1,5 @@
 TORCH=2.5
-CUDA=cu124
+CUDA=cu121
 uv pip install torch==${TORCH}.0
 uv pip install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-${TORCH}.0+${CUDA}.html
 uv pip install dgl -f https://data.dgl.ai/wheels/torch-${TORCH}/${CUDA}/repo.html
