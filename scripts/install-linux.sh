6c0f82fe52d4c0906ad0c33932996a093cce7733
Yuan Chiang <cyrusyc@berkeley.edu>
2025-04-28 19:52:21 -0700

    dgl 2.2.1

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -3,7 +3,6 @@ CUDA=cu121
 uv pip install torch==${TORCH}.0
 uv pip install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-${TORCH}.0+${CUDA}.html
 uv pip install dgl -f https://data.dgl.ai/wheels/torch-${TORCH}/${CUDA}/repo.html
-uv pip install -e .[test]
 uv pip install -e .[fairchem]
 uv pip install -e .[orb]
 uv pip install -e .[matgl]
