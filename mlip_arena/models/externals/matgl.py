cd97508b4977d1b36c7b9aba571696b86e0e6993
Yuan Chiang <cyrusyc@berkeley.edu>
2025-04-28 22:50:57 -0700

    bump matgl version and update calculator api

--- mlip_arena/models/externals/matgl.py
+++ mlip_arena/models/externals/matgl.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import matgl
 import torch
 from matgl.ext.ase import PESCalculator
+from typing import Literal
 
 
 class M3GNet(PESCalculator):
@@ -11,8 +12,10 @@ class M3GNet(PESCalculator):
         checkpoint="M3GNet-MP-2021.2.8-PES",
         # TODO: cannot assign device
         state_attr: torch.Tensor | None = None,
+        stress_unit: Literal["eV/A3", "GPa"] = "GPa",
         stress_weight: float = 1.0,
+        use_voigt: bool = False,
         **kwargs,
     ) -> None:
         potential = matgl.load_model(checkpoint)
-        super().__init__(potential, state_attr, stress_weight, **kwargs)
+        super().__init__(potential, state_attr, stress_unit, stress_weight, use_voigt, **kwargs)
--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -3,7 +3,7 @@ CUDA=cu124
 uv pip install torch==${TORCH}.0
 uv pip install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-${TORCH}.0+${CUDA}.html
 uv pip install dgl -f https://data.dgl.ai/wheels/torch-${TORCH}/${CUDA}/repo.html
-uv pip install -e .[fairchem]
+uv pip install -e .[fairchem] --no-deps
 uv pip install -e .[orb]
 uv pip install -e .[matgl]
 uv pip install -e .[test]
