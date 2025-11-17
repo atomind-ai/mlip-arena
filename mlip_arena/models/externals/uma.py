fe71ed99ecf485741bc3d9443940f5828728c6c2
Yuan Chiang <cyrusyc@berkeley.edu>
2025-11-17 14:24:05 -0800

    rename and handle dynamic device

--- mlip_arena/models/externals/uma.py
+++ mlip_arena/models/externals/uma.py
@@ -2,24 +2,29 @@ from __future__ import annotations
 
 from pathlib import Path
 
+import torch
 import yaml
-from fairchem.core import pretrained_mlip, FAIRChemCalculator
- 
+from fairchem.core import FAIRChemCalculator, pretrained_mlip
+
+from mlip_arena.models.utils import get_freer_device
 
 with open(Path(__file__).parents[1] / "registry.yaml", encoding="utf-8") as f:
     REGISTRY = yaml.safe_load(f)
 
-class UMA(FAIRChemCalculator):
+class UMA_S_1P1(FAIRChemCalculator):
     def __init__(
         self,
-        model_name=REGISTRY["UMA"]["model_name"],
-        task_name=REGISTRY["UMA"]["task_name"],
+        model_name=REGISTRY["UMA-S-1P1"]["model_name"],
+        task_name=REGISTRY["UMA-S-1P1"]["task_name"],
         cpu=False,
         **kwargs,
     ):
-        predictor = pretrained_mlip.get_predict_unit(model_name, device="cuda")
+        device = get_freer_device() if not cpu else torch.device("cpu")
+        predict_unit = pretrained_mlip.get_predict_unit(
+            model_name, 
+            device=str(device).split(":")[0]
+        )
         super().__init__(
-            predictor=predictor,
+            predict_unit=predict_unit,
             task_name=task_name,
-            device="cpu" if cpu else "cuda",
         )
