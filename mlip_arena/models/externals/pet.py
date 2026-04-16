360c2ea3d015760bb649876da585d4048216fbcd
Yuan Chiang <cyrusyc@berkeley.edu>
2026-04-15 23:10:55 -0700

    ruff format

--- mlip_arena/models/externals/pet.py
+++ mlip_arena/models/externals/pet.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import yaml
-from pathlib import Path
 
 from ase.calculators.calculator import all_changes
 from upet.calculator import UPETCalculator
@@ -17,11 +15,8 @@ class PET_OAM(UPETCalculator):
     ):
         self.device = device or str(get_freer_device())
         super().__init__(model="pet-oam-xl", version="1.0.0", device=self.device, **kwargs)
-    
-    
-    def calculate(
-        self, atoms=None, properties=['energy', 'forces', 'stress'], system_changes=all_changes
-    ):
+
+    def calculate(self, atoms=None, properties=["energy", "forces", "stress"], system_changes=all_changes):
         super().calculate(atoms, properties, system_changes)
 
 
@@ -33,9 +28,6 @@ class PET_MAD(UPETCalculator):
     ):
         self.device = device or str(get_freer_device())
         super().__init__(model="pet-mad-s", version="1.0.2", device=self.device, **kwargs)
-    
-    
-    def calculate(
-        self, atoms=None, properties=['energy', 'forces', 'stress'], system_changes=all_changes
-    ):
+
+    def calculate(self, atoms=None, properties=["energy", "forces", "stress"], system_changes=all_changes):
         super().calculate(atoms, properties, system_changes)
