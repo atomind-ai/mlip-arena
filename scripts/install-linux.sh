25c927d3d7350d1665c441eb439a7e5cc51c4088
Yuan Chiang <cyrusyc@berkeley.edu>
2026-06-08 21:16:54 -0700

    move torch_dftd and matscipy to base dep

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -4,29 +4,25 @@ if [ "$GROUP" == "nequip" ]; then
     TORCH=2.5
     CUDA=cu124
     uv pip install torch==${TORCH}.0
-    uv pip install dgl -f https://data.dgl.ai/wheels/torch-${TORCH}/${CUDA}/repo.html
     uv pip install -e .[test,extra,nequip] --no-cache
 elif [ "$GROUP" == "sevennet" ]; then
     TORCH=2.8
-    CUDA=cu128
+    CUDA=cu129
     uv pip install torch==${TORCH}.0
-    uv pip install dgl -f https://data.dgl.ai/wheels/torch-${TORCH}/${CUDA}/repo.html
     uv pip install -e .[test,extra,sevennet] --no-cache
 elif [ "$GROUP" == "mace" ]; then
     TORCH=2.8
-    CUDA=cu128
+    CUDA=cu129
     uv pip install torch==${TORCH}.0
-    uv pip install dgl -f https://data.dgl.ai/wheels/torch-${TORCH}/${CUDA}/repo.html
     uv pip install -e .[test,extra,mace] --no-cache
 elif [ "$GROUP" == "fairchem" ]; then
     TORCH=2.8
-    CUDA=cu128
+    CUDA=cu129
     uv pip install torch==${TORCH}.0
-    uv pip install dgl -f https://data.dgl.ai/wheels/torch-${TORCH}/${CUDA}/repo.html
     uv pip install -e .[test,extra,fairchem] --no-cache
 else
     TORCH=2.8
-    CUDA=cu128
+    CUDA=cu129
     uv pip install torch==${TORCH}.0
     uv pip install dgl -f https://data.dgl.ai/wheels/torch-${TORCH}/${CUDA}/repo.html
     uv pip install -e .[orb] --no-cache
--- scripts/install-perlmutter.sh
+++
@@ -1,9 +0,0 @@
-TORCH=2.9
-CUDA=cu128
-uv pip install torch==${TORCH}.0
-uv pip install dgl -f https://data.dgl.ai/wheels/torch-${TORCH}/${CUDA}/repo.html
-uv pip install -e  .[fairchem] --no-cache
-uv pip install -e  .[orb] --no-cache
-uv pip install -e  .[matgl] --no-cache
-uv pip install -e  .[test,extra,all] --no-cache
-uv pip install -e  .[mace] --no-cache
